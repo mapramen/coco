842e60a0ff71ed58701b0e0cd962781ce88a0192
Rishika Verma <river@microsoft.com>
2020-11-16 20:05:31 +0530

    final integration wih redux

--- src/Components/Board.tsx
+++ src/Components/Board.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 import '../GameApp.css';
 import Square from './Square';
 import { RootState } from "../Redux/Reducers";
+import { stat } from "fs";
 
 interface IBoardState {
     xIsNext: boolean,
@@ -11,7 +12,10 @@ interface IBoardState {
 }
 
 interface IBoardStateProps {
-    clickedSquareNumber: number
+    clickedSquareNumber: number,
+    squares: Array<string>,
+    xIsNext: boolean,
+    winner: string
 }
 
 interface IBoardDispatchProps {
@@ -23,7 +27,10 @@ interface IBoardOwnProps {
 }
 
 const mapStateToProps = (state: RootState): IBoardStateProps => ({
-    clickedSquareNumber: state.tictactoe.clickedSquareNumber
+    clickedSquareNumber: state.tictactoe.clickedSquareNumber,
+    squares: state.tictactoe.squares,
+    xIsNext: state.tictactoe.xIsNext,
+    winner: state.tictactoe.winner
 })
 
 type IBoardProps = IBoardStateProps & IBoardDispatchProps & IBoardOwnProps;
@@ -37,39 +44,26 @@ class Board extends React.Component<IBoardProps, IBoardState> {
         };
     }
 
-    handleClick(i: number) {
-        const squares = this.state.squares.slice();
-        if (calculateWinner(squares) || squares[i]) {
-            return;
-        }
-        squares[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: squares,
-            xIsNext: !this.state.xIsNext,
-        });
-    }
-
     renderSquare(i: number) {
         return (
             <Square
                 squareNumber = {i}
-                value={this.state.squares[i]}
+                value={this.props.squares[i]}
             />
         );
     }
 
     render() {
-        const winner = calculateWinner(this.state.squares);
         let status;
-        if (winner) {
-            status = 'Winner: ' + winner;
+        if (this.props.winner != "") {
+            status = 'Winner: ' + this.props.winner;
         } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+            status = 'Next player: ' + (this.props.xIsNext ? 'X' : 'O');
         }
 
         return (
             <div>
-                <div className="status">{status}</div>
+                <h1 className="status">{status}</h1>
                 <h1>Player Clicked: {this.props.clickedSquareNumber}</h1>
                 <div className="board-row">
                     {this.renderSquare(0)}
@@ -89,27 +83,6 @@ class Board extends React.Component<IBoardProps, IBoardState> {
             </div>
         );
     }
-}
-
-function calculateWinner(squares : Array<string>) {
-    const lines = [
-        [0, 1, 2],
-        [3, 4, 5],
-        [6, 7, 8],
-        [0, 3, 6],
-        [1, 4, 7],
-        [2, 5, 8],
-        [0, 4, 8],
-        [2, 4, 6],
-    ];
-
-    for (let i = 0; i < lines.length; i++) {
-        const [a, b, c] = lines[i];
-        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-            return squares[a];
-        }
-    }
-    return null;
 }
 
 export default connect<
